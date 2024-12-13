993990992a080adbae841a1b47e602f39a1e8dcd
Abdul Rafay <abdulrafay.xwave@gmail.com>
2024-12-13 10:23:31 +0500

    start project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+
+
+function App() {
+  return (
+   <>
+<h2></h2>
+   </>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import App from './App';
+
+
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <App/>
+  // <Provider store={store}>
+  //   <RouterProvider router={router} />
+  // </Provider>
+);
+
+
